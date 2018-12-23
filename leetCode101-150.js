fd0da7fafa002347d4748ffb8c38321fb3ded17f
LuckyXun <LuckyXun>
2018-12-23 17:13:40 +0800

    147题，链表排序，复杂度要求n logn，使用归并排序

--- leetCode101-150.js
+++ leetCode101-150.js
@@ -967,3 +967,75 @@ var insertionSortList = function(head) {
   })
   return nums[0]
 };
+//148. Sort List
+var sortList = function(head) {
+  if(head===null){
+      return head;
+  }
+  ret = mergeSort(head);
+  return ret;
+
+  function mergeSort(items){
+      if(items.next===null){
+          return items;
+      }
+      var left = items;
+      var right = getMid(items);
+      return　merge(mergeSort(left), mergeSort(right));
+  }
+
+  function merge(left,right){
+      var merge_result;
+      if(left===null){
+          return right;
+      }else if(right===null){
+          return left;
+      }
+      if(left.val<right.val){
+          lead = left;
+          left = left.next;
+      }else{
+          lead = right;
+          right = right.next;
+      }
+      var tmp = lead;
+      while(left!==null && right!==null){
+          if(left.val<right.val){
+              tmp.next = left;
+              tmp = left;
+              left = left.next;
+          }else{
+              tmp.next = right;
+              tmp = right;
+              right = right.next;
+          }
+      }
+      if(left!==null){
+          tmp.next = left;
+      }else{
+          tmp.next = right;
+      }
+      return lead;
+  }
+
+  function getMid(first){
+      var fast = first.next;
+      var slow = first.next;
+      var prev = first;
+      while(true)
+      {
+          if(fast !== null)
+              fast = fast.next;
+          else
+              break;
+          if(fast !== null)
+              fast = fast.next;
+          else
+              break;
+          prev = slow;
+          slow = slow.next;
+      }
+      prev.next = null;  // cut
+      return slow;
+  }
+};
